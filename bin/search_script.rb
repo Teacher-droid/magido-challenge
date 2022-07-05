1f85b37bc9e1778c7e2a50c4f13909fb9458f27a
Teacher-droid <teaching.know@gmail.com>
2022-07-06 01:14:27 +0200

    instantiating confirmation_input, validate_site_input, validate_query_input variables, editing as well error messages in both lines 67 and 75 to be more accurent & adding the method open_browser to website_generator's parameters and i tried as well to change the condition in lines 64 & 72 to gathering user input length but i couldn't figure out the correct syntax so i refrain and turn it back to true

---
+++ bin/search_script.rb
@@ -0,0 +1,83 @@
+require_relative '../lib/website_generator'
+
+class SearchScript
+  def initialize
+    welcome
+    ask_site
+    ask_query
+    ask_confirmation
+    open_browser
+  end
+
+  private
+
+  def welcome
+    p 'Welcome in our program'
+    p 'This amazing tool can search the web for you safely'
+    p 'Please tell us which site you choose'
+  end
+
+  def ask_confirmation
+    p website_url
+    p 'Do you confirm the search ?'
+    # TO COMPLETE
+    confirmation_input= gets
+    # DECLARE VARIABLE BEFORE SO CONDITION WORKS
+
+
+
+    return if confirmation_input
+
+    error(message: 'You did not confirm search')
+  end
+
+  def open_browser
+    website_generator.open
+  end
+
+  def website_url
+    website_generator.url
+  end
+
+  def website_generator
+    @website_generator ||= WebsiteGenerator.new(ask_site, ask_query, open_browser)
+  end
+
+  def ask_site
+    # TO COMPLETE
+    #
+    site_input= gets
+    # DECLARE VARIABLE BEFORE SO CONDITION WORKS
+    validate_site_input(site_input)
+  end
+
+  def ask_query
+    # TO COMPLETE
+    #
+    query_input= gets
+    # DECLARE VARIABLE BEFORE SO CONDITION WORKS
+    validate_query_input(query_input)
+  end
+
+  def validate_query_input(input)
+    # CHANGE NEXT LINE CONDITION (ex: length max / min)
+    return if true
+
+    # CHANGE NEXT LINE MESSAGE TO GIVE MORE INFORMATION ABOUT WRONG INPUT
+    error(message: "Your keyword is too short")
+  end
+
+  def validate_site_input(input)
+    # CHANGE NEXT LINE CONDITION (ex: uniq word, length max min)
+    return if true
+
+    # CHANGE NEXT LINE MESSAGE TO GIVE MORE INFORMATION ABOUT WRONG INPUT
+    error(message: "This URL is to long")
+  end
+
+  def error(message: '')
+    raise StandardError, "Oups, something happened: #{message}"
+  end
+end
+
+SearchScript.new
